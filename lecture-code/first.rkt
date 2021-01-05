44905d9347410cb1613242c6539cbe73153ccd5d
David Van Horn <dvanhorn@cs.umd.edu>
2021-01-05 11:38:31 -0500

    Code from class.

---
+++ lecture-code/first.rkt
@@ -0,0 +1,61 @@
+#lang racket
+
+;; This is the program we wrote in class, but without any embedded images
+;; so the source remains in a human-readable format
+
+;; see first-embed.rkt for the one with the embedded images
+
+(require 2htdp/image)
+
+(+ 1 2)
+
+(string-append "fred" "wilma")
+
+(define x 3.14)
+
+(define (f y)
+  (expt 2 y))
+
+
+(define spaceship (bitmap/url "https://www.cs.umd.edu/class/winter2021/cmsc388Q/shipBlue_manned.png"))
+
+
+(overlay (scale .7 spaceship)
+         (rectangle 400 200 "solid" "yellow"))
+
+
+;; (Cons Integer Color) -> Image
+(define (spin i)
+  (overlay (rotate (car i) spaceship)
+           (empty-scene 300 300 (cdr i))))
+
+(require 2htdp/universe)
+
+;(animate spin)
+
+(define (reset i ke)
+  (match ke
+    [" " (cons 0 (cdr i))]
+    ["r" (cons (car i) "red")]
+    ["y" (cons (car i) "yellow")]
+    ["p" (cons (car i) "purple")]
+    [_ i]))
+
+(define (add5 x)
+  (cons (+ (car x) 5)
+        (cdr x)))
+
+(big-bang (cons 0 "white")
+  [on-draw spin]
+  [on-tick add5 1/28]
+  [on-key reset])
+
+
+
+
+
+
+
+
+
+  
