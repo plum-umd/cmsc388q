5491aad2fc37c11f4fbb2641d8443486e8d70ac1
fzahid1 <49577696+fzahid1@users.noreply.github.com>
2021-01-20 16:01:20 -0500

    basic calculator program

---
+++ Calculator/calculator.rkt
@@ -0,0 +1,22 @@
+#lang racket/gui
+
+(provide push-equal)
+
+(define (string->f op)
+  (match op
+    ["+" +]
+    ["-" -]
+    ["*" *]
+    ["/" /]))
+
+(define (push-equal expr)
+  (define ops-lst (regexp-match
+                   #px"(\\d+[./]?\\d*)\\s*([-÷+×])\\s*(\\d+[./]?\\d*)"
+                   expr))
+  (define result 0)
+  (match ops-lst
+    [(list _ x f y)
+     (number->string ((string->f f)
+                      (string->number x)
+                      (string->number y)))]
+    [_ (raise exn:fail:user)]))
