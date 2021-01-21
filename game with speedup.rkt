699840b0c1811f911bae8e361b2c3e18ae7a5558
rschmid4 <77022627+rschmid4@users.noreply.github.com>
2021-01-21 18:44:02 -0500

    Add files via upload

--- game with speedup.rkt
+++ game with speedup.rkt
@@ -1,23 +1,18 @@
 #lang racket
 (require 2htdp/image)
-
+;;struct that keeps track of the x and y cord of the box, score, if the box has already been clicked, and time passed
 (struct game (x y score clk time))
 
-;; Launch a spinning blue spaceship
+;; starts game at a random location
 (module* main #f
   (big-bang (game (random -275 275) (random -275 275) 0 #t 0)
     [on-draw spin]
     [on-tick endG 0.1 100]
-    [on-key reset]
     [on-mouse move]))
-
-
-(define spaceship
-  (scale .5 (bitmap/url "https://www.cs.umd.edu/class/winter2021/cmsc388Q/shipBlue_manned.png")))
-
+;;defines what a box is
 (define box (square 50 "solid" "red"))
 
-
+;;sets at which time the box changes to a new random location
 (define (endG i) (if (= 10 (game-time i))(game (random -275 275) (random -275 275) (game-score i) #t (+ 1 (game-time i)))
                      (if (= 20 (game-time i)) (game (random -275 275) (random -275 275) (game-score i) #t (+ 1 (game-time i)))
                          (if (= 28 (game-time i)) (game (random -275 275) (random -275 275) (game-score i) #t (+ 1 (game-time i)))
@@ -33,20 +28,15 @@
                          (game (game-x i) (game-y i) (game-score i) (game-clk i) (+ (game-time i) 1)))))))))))))))
 
 
-;; (Cons Coordinates Color) -> Image
+;; display box at the random location, if times up display your score
 (define (spin i)
   (if (<= (game-time i) 99) (overlay/offset box (game-x i) (game-y i) (empty-scene 600 600 "white"))
       (overlay (text (string-append "You scored: " (number->string (game-score i))) 22 "red") (empty-scene 600 600 "white"))))
 
 (require 2htdp/universe)
 
-;(animate spin)
 
-(define (reset i ke)
-  (match ke
-    [_ i]))
-
-
+;; when mouse button is clicked checks to see if its within the bounds of the box and hasnt already been clicked, if so adds a point
 (define (move i x y event)
   (match event
     ["button-down" (if (game-clk i)(if (and (and (> x (- 275 (game-x i))) (< x (- 325 (game-x i))))
--- ping pong.rkt
+++ ping pong.rkt
@@ -1,12 +1,14 @@
 #lang racket
 (require 2htdp/image)
 (require 2htdp/universe)
+;;defines the ball and side pieces
 (define ball (circle 20 "solid" "red"))
 (define rect (rectangle 10 80 "solid" "black"))
-
+;;input for number of points being played to
 (print "How many points do you want to play to? ")
 (define end (string->number (read-line)))
-
+;;structure that keeps track of the xy cords of the ball, what is being added to those cords, y cords of the boxes, points of each player,
+;; and what should be added to the y cords of each players box
 (struct inf (x y mx my p1y p2y points1 points2 add1 add2))
 (module* main #f
   (big-bang (inf 0 0 5 5 0 0 0 0 0 0)
@@ -15,6 +17,7 @@
     [on-release stop]
     [on-tick move 1/60]))
 
+;;checks if either player has won, if so stop showing the game and display who won, otherwise display the game with score on the top
 (define (pos i) (if (= (inf-points1 i) end) (overlay (text "Player 1 Won!" 50 "green") (empty-scene 1000 600 "white"))
                     (if(= (inf-points2 i) end) (overlay (text "Player 2 Won!" 50 "green") (empty-scene 1000 600 "white"))
                     (overlay/offset (text (number->string (inf-points1 i)) 30 "blue") 50 270
@@ -22,6 +25,8 @@
                                     (overlay/offset rect 495 (inf-p2y i)
                                 (overlay/offset rect -495 (inf-p1y i) (overlay/offset ball (inf-x i) (inf-y i) (empty-scene 1000 600 "white")))))))))
 
+;;if game is over, do nothing, if at either end of the game, call reset which resets the game and adds a point to the appropiate player
+;;if the ball hits a paddle, ceiling, or roof bounce off
 (define (move j) (define i (addp j))
         (if (or (= (inf-points1 i) end) (= (inf-points2 i) end)) i
         (if (or (<= (inf-x i) -480) (>= (inf-x i) 480)) (reset i)
@@ -33,6 +38,7 @@
             (inf (+ (inf-x i) (inf-mx i)) (- (inf-y i) (inf-my i)) (inf-mx i) (* -1 (inf-my i)) (inf-p1y i) (inf-p2y i) (inf-points1 i) (inf-points2 i) (inf-add1 i) (inf-add2 i))
         (inf (+ (inf-x i) (inf-mx i)) (+ (inf-y i) (inf-my i)) (inf-mx i) (inf-my i) (inf-p1y i) (inf-p2y i) (inf-points1 i) (inf-points2 i) (inf-add1 i) (inf-add2 i))))))))
 
+;;when appropiate key is pressed, start adding to the y cord of the box. only does this if it wont cause paddle to go off screen
 (define (paddle i key)
   (match key
     ["w" (if (< (inf-p2y i) 260) (inf (inf-x i) (inf-y i) (inf-mx i) (inf-my i) (inf-p1y i) (inf-p2y i) (inf-points1 i) (inf-points2 i) (inf-add1 i) 15) i)]
@@ -40,9 +46,11 @@
     ["up" (if (< (inf-p1y i) 260) (inf (inf-x i) (inf-y i) (inf-mx i) (inf-my i) (inf-p1y i) (inf-p2y i) (inf-points1 i) (inf-points2 i) 15 (inf-add2 i)) i)]
     ["down" (if (> (inf-p1y i) -260) (inf (inf-x i) (inf-y i) (inf-mx i) (inf-my i) (inf-p1y i) (inf-p2y i) (inf-points1 i) (inf-points2 i) -15 (inf-add2 i)) i)]
     [_ i]))
+;;resets game and gives point to appropiate player
 (define (reset i) (if (>= (inf-x i) 480) (inf 0 0 5 5 0 0 (inf-points1 i) (+ 1 (inf-points2 i)) 0 0)
                       (inf 0 0 5 5 0 0 (+ 1 (inf-points1 i)) (inf-points2 i) 0 0)))
 
+;;when player lets go of appropiate key stop adding to the y cord of the boc
 (define (stop i key)
   (match key
     ["w" (inf (inf-x i) (inf-y i) (inf-mx i) (inf-my i) (inf-p1y i) (inf-p2y i) (inf-points1 i) (inf-points2 i) (inf-add1 i) 0)]
@@ -51,6 +59,7 @@
     ["down" (inf (inf-x i) (inf-y i) (inf-mx i) (inf-my i) (inf-p1y i) (inf-p2y i) (inf-points1 i) (inf-points2 i) 0 (inf-add2 i))]
     [_ i]))
 
+;;adds to the paddles height if it wont cause the box to go out of the screen
 (define (addp i) (if (and (canAdd (inf-p1y i) (inf-add1 i)) (canAdd (inf-p2y i) (inf-add2 i)))
   (inf (inf-x i) (inf-y i) (inf-mx i) (inf-my i) (+ (inf-p1y i) (inf-add1 i)) (+ (inf-p2y i) (inf-add2 i))
                       (inf-points1 i) (inf-points2 i) (inf-add1 i) (inf-add2 i))
@@ -59,4 +68,5 @@
   (if (canAdd (inf-p2y i) (inf-add2 i)) (inf (inf-x i) (inf-y i) (inf-mx i) (inf-my i) (inf-p1y i) (+ (inf-p2y i) (inf-add2 i))
                       (inf-points1 i) (inf-points2 i) (inf-add1 i) (inf-add2 i)) i))))
 
-  (define (canAdd y add) (if (and (> (+ y add) -260) (< (+ y add) 260)) #t #f))
+;;returns true if adding to the y cord wont cause the paddle to go off screen
+(define (canAdd y add) (if (and (> (+ y add) -260) (< (+ y add) 260)) #t #f))
