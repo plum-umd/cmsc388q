84aadfefdb90b968e9b5ebf745348fd0d2925664
mgilder <matthewgildersleeve@gmail.com>
2021-01-22 09:46:27 -0500

    more updates

--- games/mgilder-game.rkt
+++ games/mgilder-game.rkt
@@ -10,13 +10,13 @@
 (define TEXT-SIZE 30)
 (define TEXT-COLOR "darkgreen")
 (define BACKGROUND-COLOR "white")
-(define METEOR-COLOR "brown")
+(define METEOR-COLOR "purple")
 (define LASER-COLOR "blue")
 (define HEIGHT 900)
 (define WIDTH  900)
 
 (define SPIN-SPEED 4)
-(define METEOR-SPEED 1)
+(define METEOR-SPEED 3)
 (define METEOR-SIZE 30)
 (define SPAWN-DISTANCE (exact-floor (/ WIDTH 1.3)))
 
@@ -53,11 +53,21 @@
 
 ;; Launch a spinning blue spaceship
 (module* main #f
-  (big-bang (state 0 -9999 (cons (meteor -60 0) '()) '())                ;(cons 0 "white")
+  (big-bang (state 0 -9999 (cons (meteor 100 0) (cons (meteor -60 0) '())) '())                ;(cons 0 "white")
     [on-draw handleDraw]
     [on-tick handleTick]
+    [stop-when handleDead]
     [on-key  handleKey]))
 
+(define (checkMeteorDead m t)
+    (match m
+        ['() #f]
+        [z (<= (gmeteor-dist z t) (* METEOR-SIZE 1.5))]))
+
+(define (handleDead gameState)
+    (match gameState
+        [(state t l mlist elist)
+            (> (length (filter (lambda (mm) (checkMeteorDead mm t)) mlist)) 0)]))
 
 
 ;; handleDraw
@@ -70,7 +80,7 @@
     (match gameState
         [(state t l mlist elist) 
     (place-image/align (text (string-append "SCORE: " (number->string (exact-floor (/ t 60)))) TEXT-SIZE TEXT-COLOR) 0 HEIGHT "left" "bottom"
-        (overlay (rotate t spaceship)
+        (overlay (rotate (* t SPIN-SPEED) spaceship)
             (drawEffects elist
                 (drawMeteors t mlist
                     (empty-scene WIDTH HEIGHT BACKGROUND-COLOR)))))]))
@@ -83,7 +93,7 @@
 
 (define (handleTick gameState)
     (match gameState
-        [(state t l mlist elist) (state (+ t SPIN-SPEED) l mlist elist)]))
+        [(state t l mlist elist) (state (+ t 1) l mlist elist)]))
 
 
 
@@ -103,38 +113,31 @@
     base
 )
 
+(define (gmeteor-dist m t)
+    (match m
+        [(meteor d b)
+            (-
+                SPAWN-DISTANCE 
+                (* (- t b) METEOR-SPEED))]))
+
 (define (gmeteor-x m t)
     (match m
         [(meteor d b) (+
                         (* 
-                            (-
-                                SPAWN-DISTANCE 
-                                (* (- t b) METEOR-SPEED))
+                            (gmeteor-dist m t)
                             (cos (* d (/ pi 180))) )
-                        (/ WIDTH 2))]
-    )
-)
+                        (/ WIDTH 2))]))
 
 (define (gmeteor-y m t)
     (match m
         [(meteor d b) (+ 
                         (* 
-                            (- 
-                                SPAWN-DISTANCE 
-                                (* (- t b) METEOR-SPEED))
+                            (gmeteor-dist m t)
                             (sin (* d (/ pi 180))) )
-                        (/ HEIGHT 2))]
-    )
-)
+                        (/ HEIGHT 2))]))
 
 (define (drawMeteors t el base) 
     (match el
         ['() base]
-        [(cons e es) (place-image METEOR-IMAGE (gmeteor-x e t) (gmeteor-y e t) (drawMeteors t es base))]
-    )
-)
+        [(cons e es) (place-image METEOR-IMAGE (gmeteor-x e t) (gmeteor-y e t) (drawMeteors t es base))]))
 
-
-(define (add5 x)
-  (cons (+ (car x) 5)
-        (cdr x)))
