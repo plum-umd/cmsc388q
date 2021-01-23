54e87469550c1a90905522d79c1d4ae8fa00f851
Andrew Hyatt <4400272+ahyattdev@users.noreply.github.com>
2021-01-22 21:42:49 -0500

    Keypress moving code

--- opengl/square.rkt
+++ opengl/square.rkt
@@ -3,7 +3,9 @@
 (require opengl)
 (require opengl/util)
 
-(define (draw-cube)
+(define KEYMOVE 0.1)
+
+(define (draw-square x y)
   (glClearColor 0.0 0.0 0.0 0.0)
   (glClear GL_COLOR_BUFFER_BIT)
   (glColor3d 0.7 0.4 0.8)
@@ -15,10 +17,10 @@
   (glLoadIdentity)
 
   (glBegin GL_QUADS)
-  (glVertex3d 0.25 0.25 0.0)
-  (glVertex3d 0.75 0.25 0.0)
-  (glVertex3d 0.75 0.75 0.0)
-  (glVertex3d 0.25 0.75 0.0)
+  (glVertex3d (+ 0.25 x) (+ 0.25 y) 0.0)
+  (glVertex3d (+ 0.75 x) (+ 0.25 y) 0.0)
+  (glVertex3d (+ 0.75 x) (+ 0.75 y) 0.0)
+  (glVertex3d (+ 0.25 x) (+ 0.75 y) 0.0)
   (glEnd)
   )
 
@@ -30,7 +32,7 @@
    (define/override (on-paint)
       (with-gl-context
         (lambda ()
-          (draw-cube)
+          (draw-square (get-field x this) (get-field y this))
           (swap-gl-buffers)
         )
       )
@@ -43,6 +45,15 @@
             )
         )
     )
+    (init-field [x 0])
+    (init-field [y 0])
+    (define/override (on-char ch)
+      (cond
+        [(eq? (send ch get-key-code) 'left)  (print "left")];(set-field!  x (- x KEYMOVE) this)]
+        [(eq? (send ch get-key-code) 'right)  (set-field! x (+ x KEYMOVE) this)]
+        [(eq? (send ch get-key-code) 'down)  (set-field! y (+ x KEYMOVE) this)]
+        [(eq? (send ch get-key-code) 'up)  (set-field! y (- x KEYMOVE) this)]
+        [else ch]))
     (super-instantiate () (style '(gl)))
   )
 )
@@ -55,6 +66,3 @@
        [stretchable-width #f]
        [stretchable-height #f]))
 (send frame show #t)
-
-
-change this into a generic model loader
