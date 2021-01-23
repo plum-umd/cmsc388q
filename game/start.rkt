a7aea1983580e8afd7dbf1e08cc3f47ac78868d4
jgreen26 <jgreen26@terpmail.umd.edu>
2021-01-23 02:54:47 -0500

    final push

--- game/start.rkt
+++ game/start.rkt
@@ -10,10 +10,15 @@
 
 
 (define (start-game)
-  (big-bang (game (cons 20 20) 0)
+  (big-bang (game (cons 20 20) cns)
     [on-key   game-press]
     [to-draw  game-draw]
   ))
+
+;Coin locations
+(define cns '((235 . 325) (155 . 50) (100 . 54) (120 . 100) (50 . 200) (345 . 115) (275 . 279) (407 . 333) (459 . 100) (255 . 432)))
+(define NUMC 10)
+(define points 0)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -26,6 +31,8 @@
 
 ;; type Coins = Int
 
+;; type Pts = Int
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Game
 
@@ -34,16 +41,23 @@
   (match g
     [(game pos coins)
      (match ke
-       ["left"  (game (cons (- (car pos) 5) (cdr pos)) coins)]
-       ["right" (game (cons (+ (car pos) 5) (cdr pos)) coins)]
-       ["up"    (game (cons (car pos) (- (cdr pos) 5)) coins)]
-       ["down"  (game (cons (car pos) (+ (cdr pos) 5)) coins)]
+       ["left"  (game (cons (- (car pos) 5) (cdr pos)) (checkcoins pos coins))]
+       ["right" (game (cons (+ (car pos) 5) (cdr pos)) (checkcoins pos coins))]
+       ["up"    (game (cons (car pos) (- (cdr pos) 5)) (checkcoins pos coins))]
+       ["down"  (game (cons (car pos) (+ (cdr pos) 5)) (checkcoins pos coins))]
        )]
     ))
 
+;;Get rid of collected coins
+(define (checkcoins pos coins)
+  ;(print coins)
+  (filter (λ (i) (let ((dis (sqrt (+ (expt (- (car i) (car pos)) 2) (expt (- (cdr i) (car pos)) 2))))) (if (< dis 20)
+      (let ((one (add1 points))) #t)
+      #f))) coins))
+
+
 (module+ test
-  (check-equal? (game-press (game (cons 20 20) 0) "left") "left"))
-                
+)                
 
 
 
@@ -56,5 +70,18 @@
     [(game pos coins)
      (match pos
        [(cons x y) (place-image (square 20 "solid" "red") x y 
-                                (place-image (square 21 "outline" "black") x y (empty-scene 500 500)))]
+                                (place-image (square 21 "outline" "black") x y (drawcoins coins)))]
   )]))
+
+;;Compose place-image calls to draw the coins
+(define (drawcoins coins)
+  (match coins
+    ['() (empty-scene 500 500)]
+    [(cons (cons x y) rest) (place-image (circle 15 "solid" "yellow") x y (drawcoins rest))]
+    )
+  )
+
+
+
+
+
